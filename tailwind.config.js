a1af9ad2d00397d7eb4d6ae09393f8a86c787a34
Mark Meyer <markmeyer@macbook-air.myfiosgateway.com>
2022-05-18 21:50:12 -0400

    pfp rounded and placed center

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,33 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          '"Inter"',
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          '"Segoe UI"',
+          "Roboto",
+          '"Helvetica Neue"',
+          "Arial",
+          '"Noto Sans"',
+          "sans-serif",
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+      },
+      colors: {
+        "popcorn-red": "#953300",
+        "popcorn-beige": "#f5f3e2",
+        "popcorn-dark-beige": "#e5d9c9",
+        "popcorn-button-red": "#a0512d",
+        "popcorn-accent-red": "#c7754c",
+      },
+    },
   },
   plugins: [],
 };
