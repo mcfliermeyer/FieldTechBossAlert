6b5f10ee2c39b6bec945ed51051071f348539b96
Mark Meyer <markmeyer@marks-air.myfiosgateway.com>
2022-05-30 12:41:58 -0400

    wrapped node logic in functions, installed nodemon

--- src/texter.mjs
+++ src/texter.mjs
@@ -1,5 +1,5 @@
 /////////////////////node, express, and twilio things
-import dotenv from "dotenv/config";
+import dotenv from "dotenv/config"; //keep in order to load env variables
 import express from "express";
 import twilio from "twilio";
 import path from "path";
@@ -10,32 +10,34 @@ const __dirname = path.dirname(
 const img__dirname = path.dirname(
   "/Users/markmeyer/code/FieldTechBossAlert/src/img/PottsProfileLinkedIn.webp"
 );
-
-
 const app = express();
 const port = 3001;
 
-// const accountSid = process.env.TWILIO_ACCOUNT_SID;
-// const authToken = process.env.TWILIO_AUTH_TOKEN;
-// const client = twilio(accountSid, authToken);
+function sendText() {
+  const accountSid = process.env.TWILIO_ACCOUNT_SID;
+  const authToken = process.env.TWILIO_AUTH_TOKEN;
+  const client = twilio(accountSid, authToken);
 
-//sending a message to my phone from twilio
-// client.messages
-//   .create({
-//     body: "This is the ship that made the Kessel Run in fourteen parsecs?",
-//     from: "+19285855151",
-//     to: "+13307807602",
-//   })
-//   .then((message) => console.log(message.sid));
+  client.messages
+    .create({
+      body: "This is the ship that made the Kessel Run in fourteen parsecs?",
+      from: "+19285855151",
+      to: "+13307807602",
+    })
+    .then((message) => console.log(message.sid));
+}
 
-app.use(express.static(__dirname));
-app.use(express.static(img__dirname));
+function loadPageAndResources() {
+  app.use(express.static(__dirname)); //serves dist folder
+  app.use(express.static(img__dirname)); //serves img folder
 
-app.get("/", (req, res) => {
-  //serves html file to client
-  res.sendFile("/index.html", { root: "./src" });
-});
+  app.get("/", (req, res) => {
+    res.sendFile("/index.html", { root: "./src" }); //serves html file to client
+  });
 
-app.listen(port, () => {
-  console.log("listening to port http://localhost:3001");
-});
+  app.listen(port, () => {
+    console.log("listening to port http://localhost:3001");
+  });
+}
+
+loadPageAndResources()
